5be23fa76aa5308998bac5096bdefe18613617e1
Tushar <44232688+contacttushar@users.noreply.github.com>
2020-04-23 18:45:43 +0530

    Arrays -> RotateArray.java

--- src/arrays/ReverseArray.java
+++ src/arrays/ReverseArray.java
@@ -5,17 +5,17 @@ public class ReverseArray {
     public static void main(String[] args){
 
         int arr[] = {1,2,3,4,5,6,7,8,9};
-        int i = 0, j = arr.length -1, temp = 0;
+        int start = 0, end = arr.length -1, temp = 0;
 
-        while(i < j){
-            temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            i++;
-            j--;
+        while(start < end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
 
-        for (i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++){
             System.out.print(arr[i]+" "); //9 8 7 6 5 4 3 2 1
         }
     }
---
+++ src/arrays/RotateArray.java
@@ -0,0 +1,34 @@
+package arrays;
+
+public class RotateArray {
+
+    public static void main(String[] args) {
+
+        int arr[] = {1,2,3,4,5,6,7,8};
+        int d = 2;
+
+        rotateTheArrayByD(arr, d);
+
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]+" "); //3 4 5 6 7 8 1 2
+        }
+    }
+
+    static void rotateTheArrayByD(int arr[], int d){
+
+        reverse(arr, 0, d-1);
+        reverse(arr, d, arr.length - 1);
+        reverse(arr, 0, arr.length -1);
+    }
+
+    static void reverse(int arr[], int start, int end){
+
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
